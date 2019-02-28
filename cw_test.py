b5a00de76e51ea36b8f526e54b25d6841e0cada7
annu <annusingh100995>
2019-02-28 13:39:49 +0000

    Thrusday

--- cw_test.py
+++ cw_test.py
@@ -1,7 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import tkinter
+import scipy
 from scipy.integrate import odeint
-
+import matplotlib.pyplot as plt
 
 np.random.seed(1000)
 
@@ -101,35 +102,42 @@ def compute_derivatives(y, t0):
 
     return dy
 
-# State (Vm, n, m, h)
-Y = np.array([0.0, n_inf(), m_inf(), h_inf()])
+def main():
 
-# Solve ODE system
-# Vy = (Vm[t0:tmax], n[t0:tmax], m[t0:tmax], h[t0:tmax])
-Vy = odeint(compute_derivatives, Y, T)
+    # State (Vm, n, m, h)
+    Y = np.array([0.0, n_inf(), m_inf(), h_inf()])
+
+    # Solve ODE system
+    # Vy = (Vm[t0:tmax], n[t0:tmax], m[t0:tmax], h[t0:tmax])
+    Vy = odeint(compute_derivatives, Y, T)
 
 
-Idv = [Id(t) for t in T]
+    Idv = [Id(t) for t in T]
 
-fig, ax = plt.subplots(figsize=(12, 7))
-ax.plot(T, Idv)
-ax.set_xlabel('Time (ms)')
-ax.set_ylabel(r'Current density (uA/$cm^2$)')
-ax.set_title('Stimulus (Current density)')
-plt.grid()
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax.plot(T, Idv)
+    ax.set_xlabel('Time (ms)')
+    ax.set_ylabel(r'Current density (uA/$cm^2$)')
+    ax.set_title('Stimulus (Current density)')
+    plt.grid()
+    plt.show()
 
-# Neuron potential
-fig, ax = plt.subplots(figsize=(12, 7))
-ax.plot(T, Vy[:, 0])
-ax.set_xlabel('Time (ms)')
-ax.set_ylabel('Vm (mV)')
-ax.set_title('Neuron potential with two spikes')
-plt.grid()
+    # N euron potential
+    fig, ax = plt.subplots(figsize=(12, 7))
+    ax.plot(T, Vy[:, 0])
+    ax.set_xlabel('Time (ms)')
+    ax.set_ylabel('Vm (mV)')
+    ax.set_title('Neuron potential with two spikes')
+    plt.grid()
+    plt.show()
 
-# Trajectories with limit cycles
-fig, ax = plt.subplots(figsize=(10, 10))
-ax.plot(Vy[:, 0], Vy[:, 1], label='Vm - n')
-ax.plot(Vy[:, 0], Vy[:, 2], label='Vm - m')
-ax.set_title('Limit cycles')
-ax.legend()
-plt.grid()
+    # Trajectories with limit cycles
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.plot(Vy[:, 0], Vy[:, 1], label='Vm - n')
+    ax.plot(Vy[:, 0], Vy[:, 2], label='Vm - m')
+    ax.set_title('Limit cycles')
+    ax.legend()
+    plt.grid()
+    plt.show()
+
+main()
