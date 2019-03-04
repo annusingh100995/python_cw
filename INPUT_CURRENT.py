6adba5b182438da634a95350c74b902303d72c02
annu <annusingh100995>
2019-03-04 13:04:27 +0000

    Monday

--- INPUT_CURRENT.py
+++ INPUT_CURRENT.py
@@ -21,6 +21,20 @@ class Current:
             self.time_step = input()
             return self.time_step
 
+      def Step_current(self):
+          current = np.zeros(len(T))
+          current_length = len(current)
+          window_size = self.time_step
+          number_of_windows = current_length/window_size
+          updated_current = current
+          number_list = np.arange(1,number_of_windows,2)
+          for i in number_list:
+              for j in range(1,window_size):
+                  temp = int(((i*window_size)+j))
+                  updated_current[temp] = 10
+
+
+
 
 def main():
     current = Current()
